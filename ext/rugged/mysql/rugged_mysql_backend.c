74f6fe232307cf295eeb3156419a6064132fe735
Rohit Garg <cryptolemy+git@gmail.com>
2015-07-22 15:36:05 -0700

    Update rugged_mysql_backend.c

--- ext/rugged/mysql/rugged_mysql_backend.c
+++ ext/rugged/mysql/rugged_mysql_backend.c
@@ -14,12 +14,12 @@ int git_odb__error_ambiguous(char *);
 int git_odb__error_notfound(char *);
 int git_odb__error_ambiguous(char *);
 
-/*typedef struct _rugged_backend {
+typedef struct _rugged_backend {
 	int (*odb_backend) (git_odb_backend ** backend_out,
 			    struct _rugged_backend * backend);
 	int (*refdb_backend) (git_refdb_backend ** backend_out,
 			      struct _rugged_backend * backend);
-} rugged_backend;*/
+} rugged_backend;
 
 typedef struct {
 	rugged_backend backend;
